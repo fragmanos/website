6ce86e902c4a5b82c64a0e00257650b91d068881
Manos Fragkakis <fragmanos.dev@gmail.com>
2021-08-23 19:34:10 +0300

    add github-readme-stats to my readme

---
+++ src/components/box.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+
+const Box = () => {
+    return (
+        <Box component="span" m={1}>
+            <Button />
+        </Box>
+    )
+}
+
+export default Box
